fd6a46398fc51e014d877de0aeef0b124f75f5b1
ggutzwiller <gregoire@gutzwiller.eu>
2019-12-22 21:05:50 +0100

    Initial commit: test classes, readme and main class

---
+++ src/main/java/com/ggutzwiller/xebia/mowitnow/Main.java
@@ -0,0 +1,23 @@
+package com.ggutzwiller.xebia.mowitnow;
+
+/**
+ * @author Grégoire Gutzwiller
+ * @date 22/12/2019
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * This method runs the program, ie. takes a string representing
+     * the instructions and returns the solution.
+     *
+     * @param inputInstructions the instructions as a string
+     * @return the solution in a string
+     */
+    public static String run(String inputInstructions) {
+        return "";
+    }
+}
---
+++ src/test/java/com/ggutzwiller/xebia/mowitnow/MowItNowDemo.java
@@ -0,0 +1,54 @@
+package com.ggutzwiller.xebia.mowitnow;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Grégoire Gutzwiller
+ * @date 22/12/2019
+ */
+@Test
+public class MowItNowDemo {
+
+    /**
+     * It is used to test the solution with Xebia's example.
+     */
+    @Test
+    public void demoXebiaExample() {
+        /* Given */
+        String theInstructionSet = "5 5\n1 2 N\nGAGAGAGAA\n3 3 E\nAADAADADDA";
+        String theSolution = "1 3 N\n5 1 E";
+
+        /* When */
+        String proposedSolution = Main.run(theInstructionSet);
+
+        /* Then */
+        Assert.assertEquals(proposedSolution, theSolution);
+    }
+
+    /**
+     * It is used to test the solution with mowers going outside grid.
+     */
+    @Test
+    public void demoOutsideGrid() {
+        String theInstructionSet = "3 3\n1 2 N\nAGAAGAADADDA\n3 3 S\nAGAGGGGA";
+        String theSolution = "1 0 E\n3 2 E";
+
+        String proposedSolution = Main.run(theInstructionSet);
+        Assert.assertEquals(proposedSolution, theSolution);
+    }
+
+    /**
+     * Used to test the solution with mowers crashing.
+     * We made the assumption that the behaviour will be the same as if the mower goes
+     * outside of the grid.
+     */
+    @Test
+    public void demoMowerCrash() {
+        String theInstructionSet = "2 2\n1 2 S\nA\n1 0 N\nADAGAAGAGAADAGAAGAG";
+        String theSolution = "1 1 S\n1 0 N";
+
+        String proposedSolution = Main.run(theInstructionSet);
+        Assert.assertEquals(proposedSolution, theSolution);
+    }
+}
